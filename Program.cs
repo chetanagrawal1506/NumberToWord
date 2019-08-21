63138f633d2780e2d6d7ec9f74255e7391dfa3f8
chetanagrawal1506 <54346199+chetanagrawal1506@users.noreply.github.com>
2019-08-21 10:53:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericToWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumber;
+            Console.Write("Enter a string - ");
+            inputNumber = Console.ReadLine();
+            var number = NumberToWords(Convert.ToInt64(inputNumber));
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+
+        public static string NumberToWords(long number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var units = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tens = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += units[number];
+                else
+                {
+                    words += tens[number / 10];
+                    if ((number % 10) > 0)
+                        words += " " + units[number % 10];
+                }
+            }
+
+            return words;
+        }
+    }
+}
